744ec660947e75fb1094564c692bfd641b75b566
Bagus Adhi Nugroho <130440664+Velkh@users.noreply.github.com>
2023-10-09 20:35:34 +0700

    Add files via upload

---
+++ AboutMe.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <Title>Bagus's Personal Web</Title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <header><h2>Bagus Adhi Nugroho</h2></header>
+        <nav>
+            <ul>
+            <li><A href="personalWeb.php">Home</A></li> 
+            <li><A href="AboutMe.php">About Me</A></li> 
+            <li><A href="portfolio.php">Portfolio</A></li> 
+            <li><A href="personalContact.php">Contact Me</A></li> 
+            </ul>    
+        </nav>
+        <hr>
+        <h2>About Me</h2>
+        <img src="myPhoto.jpeg" alt="Bagus's Photo">
+        <p= class="aboutMe1">Hi!, my name is Bagus Adhi Nugroho and you can call me Bagus. I'm a student from CCIT FTUI and State Politechnic of Jakarta. 
+            I'm graduated from SMAN 29 Jakarta, and now i trying to develop my personal Web</p>
+        <p>I have interest on Web Development and Photography. I'd like to take a photo for my hobby. 
+            and i have a dream, to be a Web Developer and i hope i can achieve my dream. </p>
+    </body>
+</html>
